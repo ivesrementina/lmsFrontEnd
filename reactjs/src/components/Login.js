57cb580bfcffc8cab830bd44da777a72fb96a210
Ives <rementinaives@gmail.com>
2023-06-28 22:23:25 +0800

    inprogress

--- reactjs/src/components/Login.js
+++ reactjs/src/components/Login.js
@@ -35,7 +35,16 @@ const Login = () => {
     setPwd('');
     setSuccess(true);
     } catch (err) {
-
+        if (!err?.response) {
+          setErrMsg('No Server Response');
+        } else if (err.response?.status === 400) {
+          setErrMsg('Missing Username or Password');
+        } else if (err.response?.status === 401) {
+          setErrMsg('Unauthorized');
+        } else {
+          setErrMsg('Login Failed');
+        } 
+        errRef.current.focus();
     }
     
   }
